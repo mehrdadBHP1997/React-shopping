ed27980a2f5892d00e8c0799b1b1944cb3dc2f58
Mehrdad Baba Hosein Pour <67967496+mehrdadBHP1997@users.noreply.github.com>
2022-05-31 11:25:03 +0430

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import "./App.css";
+import HomePage from "./pages/HomePage";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import CartPage from "./pages/CartPage";
+import CartProvider from "./Providers/CartProdvicer";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import CheckoutPage from "./pages/ChekcoutPage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import AuthProvider from "./Providers/AuthProvider";
+
+function App() {
+  return (
+    <Router>
+      <AuthProvider>
+        <CartProvider>
+          <ToastContainer />
+          <Switch>
+            <Route path="/cart" component={CartPage} />
+
+            <Route path="/checkout" component={CheckoutPage} />
+
+            <Route path="/login" component={LoginPage} />
+
+            <Route path="/signup" component={SignupPage} />
+
+            <Route path="/" component={HomePage} />
+          </Switch>
+        </CartProvider>
+      </AuthProvider>
+    </Router>
+  );
+}
+
+export default App;
